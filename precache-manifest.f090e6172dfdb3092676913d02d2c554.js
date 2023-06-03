807fb2080445903af0c5ebe504ad93f29e16569e
Ben LeVeque <ben.leveque@gmail.com>
2023-06-03 12:47:40 -0400

    Updates

--- precache-manifest.f090e6172dfdb3092676913d02d2c554.js
+++ precache-manifest.f090e6172dfdb3092676913d02d2c554.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e91e0d1c454327a4c3a502b940998fdb",
+    "revision": "751dd570f37b111dc17533f0d2ff68ca",
     "url": "/index.html"
   },
   {
-    "revision": "cf7c0cf2e1b924d97854",
+    "revision": "77d7f1db68bd719a0dd3",
     "url": "/static/css/main.e548a633.chunk.css"
   },
   {
-    "revision": "92697960300701efe439",
-    "url": "/static/js/2.d7686e39.chunk.js"
+    "revision": "ee926e5efecf6a864310",
+    "url": "/static/js/2.887860cc.chunk.js"
   },
   {
     "revision": "94b29ac6d0ec426932e82da99c88b81c",
-    "url": "/static/js/2.d7686e39.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.887860cc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cf7c0cf2e1b924d97854",
-    "url": "/static/js/main.4cbecf69.chunk.js"
+    "revision": "77d7f1db68bd719a0dd3",
+    "url": "/static/js/main.353a7c47.chunk.js"
   },
   {
     "revision": "8967fc5e35946e894c38",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ed5b95c85ce927a51958d4eec40b53b.js"
+  "/precache-manifest.f090e6172dfdb3092676913d02d2c554.js"
 );
 
 self.addEventListener('message', (event) => {
