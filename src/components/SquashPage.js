8381e1d2fc6830173a46a33fbc264cd0e1ea4cfa
Ben LeVeque <ben.leveque@gmail.com>
2023-06-03 12:05:23 -0400

    update

--- src/components/SquashPage.js
+++ src/components/SquashPage.js
@@ -10,8 +10,8 @@ const SquashPage = () => {
       <h2>Table of Contents</h2>
       <ol>
         <li><a href="#planting">Planting (5/29/23)</a></li>
-        <li><a href="#context">Context</a></li>
         <li><a href="#disclaimer">Disclaimer</a></li>
+        <li><a href="#context">Context</a></li>
       </ol>
 
       <h2 id="planting">Planting (5/29/23)</h2>
@@ -44,13 +44,13 @@ const SquashPage = () => {
           <img class="img-large" src="/squash/raised_beds_post_prep_1.jpeg" alt="Image of cleared raised beds"></img>
         </div>
 
-        This scene is likely unremarkable to a casual observer, but close inspection reveals two different colors of dirt.
+        A casual observer may find nothing remarkable in the image above, but close inspection reveals two different colors of dirt.
         This indicates something critical about the acidity, moisture level, and age -- in other words, the squash-friendliness --
-        of the soil. Expect the blog to revisit this observation frequently as we compare the growth patterns of the squash in each
-        box. Never mind that one box will hold butternut squash and the other zucchini.
+        of the soil, I'm sure. Expect the blog to revisit this observation frequently as we compare the growth patterns of the squash
+        in each box. Never mind that one box will hold butternut squash and the other, zucchini.
       </p>
       <p>
-        After clearing the boxes, it was time to put a layer of fresh soil down. I always use Organic All Natural Garden Soil for
+        After clearing the boxes, it was time to put down a layer of fresh soil. I always use Organic All Natural Garden Soil for
         this purpose:
 
         <div class="image-container">
@@ -59,42 +59,50 @@ const SquashPage = () => {
           <img class="img-large" src="/squash/raised_beds_post_soil_2.jpeg" alt="Image of beds and pot with fresh soil"></img>
         </div>
 
-        These beds are looking good! You'll notice, too, a small pot in frame. This also got a fresh layer of soil, but no, I
-        didn't plant squash in that.....
+        These beds are looking good! You'll notice, too, a small pot in frame. This also got a fresh layer of soil, but what will be planted
+        there?? No soilers!
       </p>
       <p>
-        It was now time to plant. In the left box, I planted approximately 9 zucchini seeds in a sort of geometrically optimized
-        lattice pattern. These are probably closer together than they ultimately should be, so I may trim a couple out later. In the right
-        box, I planted 4 butternut squash seeds in a squarish pattern. In the small pot in the middle, I planted several jalapeno
-        seeds in a sort of snowflake or flower pattern, with one in the middle and several closer to the edge.
+        It was now time to plant. In the left box, I buried approximately 9 zucchini seeds in a sort of geometrically optimized
+        lattice pattern. These are probably closer together than they eventually should be, so I may trim a couple out later. In the right
+        box, I planted 4 butternut squash seeds in a squarish pattern. In the small pot in the middle, I planted several jalapeño
+        seeds in a sort of snowflake or flower pattern, with one in the middle and several around the edge. You may also think of this as
+        a hub-and-spoke pattern or a pizza pie pattern. To think of it as any other type of pattern would be incorrect.
       </p>
       <p>
-        The location of the raised beds is admittedly not ideal for growing squash, as I don't believe they get as much sunlight
+        The location of the raised beds is admittedly not ideal for growing squash, as I don't believe the boxes get as much sunlight
         as squash would prefer, but we'll see.
       </p>
       <p>
         Thanks for reading, and please be sure to comment with any squash-growing tips!
       </p>
 
+      <hr />
+
+      <h2 id="disclaimer">Disclaimer</h2>
+      <p>
+        I am, at present, an unskilled and inexperienced gardener. As such, the contents of this blog shall, under no circumstances,
+        be treated as the documented wisdom of a professional or skilled amateur gardener. Any resemblance to reliable gardening advice
+        is purely coincidental. Furthermore, I shall not be held responsible, legally or ethically, for any gardening mishaps or
+        catastrophes that emanate herefrom. Thank you, and happy gardening!
+      </p>
+
+      <hr />
 
       <h2 id="context">Context</h2>
       <p>
         I recently placed last in a fantasy football league, and so I was asked to grow squash.
       </p>
       <p>
-        N.B. A previous year's league loser was asked to write a book report on David Brin's <i>Kiln People</i>;
+        In all seriousness, I think gardening is pretty cool and am excited for this challenge; I hope to do a good job.
+        This blog is just some nonsense!
+      </p>
+      <p>
+        <i>N.B.</i> A previous year's league loser was asked to write a book report on David Brin's <i>Kiln People</i>;
         the report may be found <a href="">[todo]</a>.
       </p>
-      
-      <h2 id="disclaimer">Disclaimer</h2>
-      <p>
-        I am, at present, an unskilled and inexperienced gardener, though I wish to be a skilled and experienced gardener. In
-        the meantime, the contents of this blog shall, under no circumstances, be treated as the documented wisdom of a
-        professional or skilled amateur gardener. Any resemblance to reliable gardening advice is purely coincidental. Furthermore,
-        I shall not be held responsible, legally or ethically, for any gardening mishaps or catastrophes that emanate herefrom.
-        Thank you, and happy gardening!
-      </p>
-
+      <br />
+      <br />
     </div>
   );
 }
