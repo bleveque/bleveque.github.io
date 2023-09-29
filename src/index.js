5185f910ba4aaab0d7d8f7fbdbf711bc8987364e
Ben LeVeque <ben.leveque@gmail.com>
2023-09-28 23:58:06 -0400

    archiving home page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
-import HomePage from './components/HomePage';
+import HomePage2023Archive from './archive/2023/components/HomePage';
 import SquashPage from './components/SquashPage';
 import blogContents from './squashBlogContents/index';
 import SquashBlogContents from './components/SquashBlogContents';
@@ -12,23 +12,26 @@ function App() {
   return (
     <HashRouter>
       <Switch>
-      { blogContents.map((bc, idx) => {
-        const prevLink = idx > 0 ? `/#/squash/${blogContents[idx - 1].path}` : null;
-        const nextLink = idx < blogContents.length - 1 ? `/#/squash/${blogContents[idx + 1].path}` : null;
-        return (
-          <Route path={`/squash/${bc.path}`}>
-            <BlogPage homeLink="/" prevLink={prevLink} nextLink={nextLink} tocLink="/#/squash">
-              <SquashBlogContents contentData={bc} />
-            </BlogPage>
-          </Route>
-        );
-      })}
+        { blogContents.map((bc, idx) => {
+          const prevLink = idx > 0 ? `/#/squash/${blogContents[idx - 1].path}` : null;
+          const nextLink = idx < blogContents.length - 1 ? `/#/squash/${blogContents[idx + 1].path}` : null;
+          return (
+            <Route path={`/squash/${bc.path}`}>
+              <BlogPage homeLink="/" prevLink={prevLink} nextLink={nextLink} tocLink="/#/squash">
+                <SquashBlogContents contentData={bc} />
+              </BlogPage>
+            </Route>
+          );
+        })}
         <Route path="/squash">
           <SquashPage />
         </Route>
-        <Route path="/">
-          <HomePage />
+        <Route path="/2023_archive_home">
+          <HomePage2023Archive />
         </Route>
+        {/* <Route path="/">
+          <HomePage />
+        </Route> */}
       </Switch>
     </HashRouter>
   );
